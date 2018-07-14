efffd1a9251c18e97d38c7a973c485a530aae859
CarpeSwag <james@garbagnati.com>
2018-07-13 18:02:52 -0700

    Split webpage into four main containers.

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -4,16 +4,50 @@ import {Link, NavLink} from 'inferno-router';
 export default class Home extends Component {
 	constructor(props) {
 		super(props);
+		
+		this.state = {
+			height: 0,
+			value: '',
+			currSeason: 0
+		}
+		
+		this.onFocus = this.onFocus.bind(this);
+		this.onBlur = this.onBlur.bind(this);
+		this.onInput = this.onInput.bind(this);
+	}
+	
+	onFocus() {
+		
+	}
+	
+	onBlur() {
+		
+	}
+	
+	onInput() {
+		
 	}
 	
 	render() {
 		return (
 			<div id="home-cntr">
 				<div className="left-cntr">
-				
+					<div className="header-cntr">
+						Warcraft 3 Banjoball Elo, Season {this.state.currSeason + 1}
+					</div>
 				</div>
 				<div className="right-cntr">
-				
+					<div className="search-cntr">
+						<div className="search-btn">
+							Search
+						</div>
+						<input type='text' placeholder='Player 1, Player 2, Player 3...'
+							className='search-input'
+							value={this.props.value}
+							onFocus={this.onFocus}
+							onBlur={this.onBlur}
+							onInput={this.onInput} />
+					</div>
 				</div>
 			</div>
 		);
