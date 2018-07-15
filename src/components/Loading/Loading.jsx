df68e565fc9d684034fcd945de82da81348418e3
CarpeSwag <james@garbagnati.com>
2018-07-15 01:57:57 -0700

    Changed loading animation to css.

--- src/components/Loading/Loading.jsx
+++ src/components/Loading/Loading.jsx
@@ -7,29 +7,6 @@ const DOTS = 3;
 export default class Loading extends Component {
 	constructor(props) {
 		super(props);
-		
-		this.state = {
-			loadingDots: 0
-		}
-		
-		window.requestAnimationFrame(() => this.animateLoading());
-	}
-	
-	animateLoading() {
-		this.setState({
-			loadingDots: (this.state.loadingDots + 1) 
-				% (FRAMES_PER_DOT * (DOTS + 1))
-		});
-		window.requestAnimationFrame(() => this.animateLoading());
-	}
-	
-	loadingMsg() {
-		let out = "Loading content";
-		let len = Math.floor(this.state.loadingDots / FRAMES_PER_DOT);
-		for (let i = 0; i < len; ++i) {
-			out = out + ".";
-		}
-		return out;
 	}
 	
 	render() {
@@ -37,7 +14,6 @@ export default class Loading extends Component {
 			<div id='loading-screen'>
 				<div className='loading-outer'>
 					<div className='loading-inner'>
-						<h1>{this.loadingMsg()}</h1>
 					</div>
 				</div>
 			</div>
